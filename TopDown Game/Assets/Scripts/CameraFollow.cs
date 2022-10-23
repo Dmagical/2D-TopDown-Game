6a224e513535fdefbf6a6ff9a4e330a05c3d3cb5
Dmagical <mjonespacheco.mjp@gmail.com>
2022-10-23 06:45:15 -0700

    Camera Follow Update
    Camera follow no longer jittery and follows player from center of screen

--- TopDown Game/Assets/Scripts/CameraFollow.cs
+++ TopDown Game/Assets/Scripts/CameraFollow.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float followSpeed = 5f;
-    public float yOffset = 0f;
     public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
